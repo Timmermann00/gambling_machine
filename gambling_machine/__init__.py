e85e35715c4890f00f7da6dfe899c8f33710316e
Tim Harbeck <timmermann.harbeck@gmail.com>
2025-01-08 15:18:12 +0100

    feat: reworked python-app.yml

--- gambling_machine/__init__.py
+++ gambling_machine/__init__.py
@@ -1,7 +1,7 @@
 from .user import User, read_player_stats, write_player_stats, handle_player
 from .game import rand_word_gen, play_game, print_options, handle_choice
 
-# Definiere die öffentliche API des Pakets
+
 __all__ = [
     "User",
     "read_player_stats",
