7776a7447c9bbe00577ead5442706be025a7e3f9
Tim Harbeck <timmermann.harbeck@gmail.com>
2025-01-10 10:24:11 +0100

    fix: naming

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,21 +1,21 @@
 from gambling_machine.game import define_win_multiplier
 
 def test_define_win_multiplier_apple():
-    resultList = [["Apple", "Apple", "Apple"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
-    result = define_win_multiplier(resultList)
+    result_list = [["Apple", "Apple", "Apple"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
+    result = define_win_multiplier(result_list)
     assert result == 2
 
 def test_define_win_multiplier_orange():
-    resultList = [["Orange", "Orange", "Orange"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
-    result = define_win_multiplier(resultList)
+    result_list = [["Orange", "Orange", "Orange"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
+    result = define_win_multiplier(result_list)
     assert result == 3
 
 def test_define_win_multiplier_grapefruit():
-    resultList = [["Grapefruit", "Grapefruit", "Grapefruit"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
-    result = define_win_multiplier(resultList)
+    result_list = [["Grapefruit", "Grapefruit", "Grapefruit"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
+    result = define_win_multiplier(result_list)
     assert result == 5
 
 def test_define_win_multiplier_no_fruit():
-    resultList = [["Orange", "Apple", "Grapefruit"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
-    result = define_win_multiplier(resultList)
+    result_list = [["Orange", "Apple", "Grapefruit"], ["Orange", "Apple", "Orange"], ["Orange", "Apple", "Grapefruit"]]
+    result = define_win_multiplier(result_list)
     assert result == 0
